c988af3276e240fdb7db4a4a9bc0e7ffb88c8b63
marstrx <marstrone@gmail.com>
2025-11-18 14:04:36 +0100

    models\comment.model.js

---
+++ models/comment.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      maxLength: 300,
+      trim: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    article: {
+      type: Schema.Types.ObjectId,
+      ref: "Article",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
