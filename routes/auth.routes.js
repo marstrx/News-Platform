b1eb1fc434f002815eb7d823fcbb065f19909f9d
marstrx <marstrone@gmail.com>
2025-11-26 15:38:30 +0100

    clean code

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,20 +1,10 @@
 import express from "express";
 import registerUser from "../controllers/authController/register.controller.js";
 import login from "../controllers/authController/login.controller.js";
-import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
-
-
-router.get("/profile", protect, (req, res) => {
-  res.json({
-    success: true,
-    message: "Welcome to your profile!",
-    user: req.user,
-  });
-});
 
 export default router;
