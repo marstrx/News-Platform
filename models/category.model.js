bae8b15634cca7df813eacdca1efb5f226af9e45
marstrx <marstrone@gmail.com>
2025-12-05 18:11:24 +0100

    slugify added to category

--- models/category.model.js
+++ models/category.model.js
@@ -1,19 +1,44 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const categorySchema= new Schema({
-    name :{
-        type :String,
-        required :true,
-        unique :true,
-        trim: true,
-        maxLength: 50
-    },
-    slug:{
-        type :String,
-        unique :true,
-        trim: true
-    }
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    maxLength: 50,
+  },
+  slug: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
 });
 
-const Category = mongoose.model("Category",categorySchema);
+categorySchema.pre("validate", async function (next) {
+  if (!this.name) return next();
+
+  let baseSlug = slugify(this.name, { lower: true, strict: true });
+  let slug = baseSlug;
+
+  const existingCategory = await mongoose.models.Category.findOne({ slug });
+
+  if (
+    existingCategory &&
+    existingCategory._id.toString() !== this._id.toString()
+  ) {
+    let counter = 2;
+    while (
+      await mongoose.models.Category.findOne({ slug: `${baseSlug}-${counter}` })
+    ) {
+      counter++;
+    }
+    slug = `${baseSlug}-${counter}`;
+  }
+
+  this.slug = slug;
+  next();
+});
+
+const Category = mongoose.model("Category", categorySchema);
 export default Category;
