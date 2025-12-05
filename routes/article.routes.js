f905c07d3ecb8baecb9b1056e53197414e723b69
marstrx <marstrone@gmail.com>
2025-12-05 18:56:20 +0100

    add article added to routes

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import featchAllArticles from "../controllers/articleController/article.read.js";
+import addArticle from "../controllers/articleController/article.add.js";
 
 // create route 
 const articleRouter = express.Router();
 
 articleRouter.get("/read" ,featchAllArticles);
+// Add new article
+articleRouter.post("/add" ,addArticle);
 
 export default articleRouter;
