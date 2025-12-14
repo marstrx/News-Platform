4fbd6d0ec5fcbf6482884efd42d7bfd457b76396
marstrx <marstrone@gmail.com>
2025-12-14 17:01:25 +0100

    update article simple structure

---
+++ controllers/articleController/article.update.js
@@ -0,0 +1,12 @@
+import Article from "../../models/article.model.js";
+
+
+const updateArticleById=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default updateArticleById;
