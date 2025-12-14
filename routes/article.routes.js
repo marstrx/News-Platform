6fdfb3ef551da82cf816e20704b4fb07060afee6
marstrx <marstrone@gmail.com>
2025-12-14 16:39:40 +0100

    delete article added to routes

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import featchAllArticles from "../controllers/articleController/article.read.js";
 import addArticle from "../controllers/articleController/article.add.js";
+import deleteOneArticleById from "../controllers/articleController/article.delete.js";
 
 // create route 
 const articleRouter = express.Router();
@@ -8,5 +9,7 @@ const articleRouter = express.Router();
 articleRouter.get("/read" ,featchAllArticles);
 // Add new article
 articleRouter.post("/add" ,addArticle);
+// Delete Article by id
+articleRouter.delete("/delete/:id", deleteOneArticleById);
 
 export default articleRouter;
