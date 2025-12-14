d53c56f83089fd64d59b1165642e83826a0221a8
marstrx <marstrone@gmail.com>
2025-12-14 18:16:23 +0100

    update fun added to routes

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import featchAllArticles from "../controllers/articleController/article.read.js";
 import addArticle from "../controllers/articleController/article.add.js";
 import deleteOneArticleById from "../controllers/articleController/article.delete.js";
+import updateArticleById from "../controllers/articleController/article.update.js";
 
 // create route 
 const articleRouter = express.Router();
@@ -11,5 +12,7 @@ articleRouter.get("/read" ,featchAllArticles);
 articleRouter.post("/add" ,addArticle);
 // Delete Article by id
 articleRouter.delete("/delete/:id", deleteOneArticleById);
+//  Update Article by Id
+articleRouter.put("/update/:id" ,updateArticleById);
 
 export default articleRouter;
