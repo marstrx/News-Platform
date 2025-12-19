f804d20234b45b7a93d41de1b0dcc1bb6a1965d9
marstrx <marstrone@gmail.com>
2025-12-19 21:55:48 +0100

    simple routes added

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import Header from './Layoutes/Header/Header.jsx';
+import React from "react";
+import Header from "./Layoutes/Header/Header.jsx";
+import Footer from "./Layoutes/Footer/Footer.jsx";
+import Login from "./Components/Login/Login.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
-  )
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
