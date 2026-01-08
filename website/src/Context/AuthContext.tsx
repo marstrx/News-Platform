ff148401d5799c4de432b0b7fdd6f7ba8c4d1639
marstrx <marstrone@gmail.com>
2026-01-08 13:23:22 +0100

    auth context temp

---
+++ website/src/Context/AuthContext.tsx
@@ -0,0 +1,11 @@
+import { JSX } from "react"
+import { useContext , useState , useEffect } from "react"
+import Api from "../Services/api"
+
+function AuthContext() :JSX.Element {
+  return (
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
