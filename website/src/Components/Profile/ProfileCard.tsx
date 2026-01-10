e5a9839c328a1d9f3fe0c7cfc50421ea6da5a765
marstrx <marstrone@gmail.com>
2026-01-10 19:00:14 +0100

    simple menu

--- website/src/Components/Profile/ProfileCard.tsx
+++ website/src/Components/Profile/ProfileCard.tsx
@@ -5,9 +5,15 @@ import { AuthContext } from "../../Context/AuthContext";
 function ProfileCard(): JSX.Element {
     const { logout } = useContext(AuthContext);
     return (
-        <div className="flex">
-            <Link to={"/profile"}>Profile</Link>
-            <button className="hover:underline" onClick={logout}>Logout</button>
+        <div className="flex flex-col items-end">
+            <div className="absolute bg-[#132440] text-[#FDB5CE] w-52 p-4 text-lg font-mono rounded-bl-md">
+                <div>
+                    <Link to={"/profile"}>Profile</Link>
+                </div>
+                <div>
+                    <button className="hover:underline" onClick={logout}>Logout</button>
+                </div>
+            </div>
         </div>
     )
 }
