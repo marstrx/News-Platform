0e5222f15de3a56c5238f560b3df58b823f89fda
marstrx <marstrone@gmail.com>
2026-01-13 15:23:06 +0100

    user data

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,15 +3,27 @@ import registerUser from "../controllers/authController/register.controller.js";
 import login from "../controllers/authController/login.controller.js";
 import protect from "../middlewares/auth.middleware.js";
 import logout from "../controllers/authController/logout.controller.js";
+import User from "../models/user.model.js";
 
 const router = express.Router();
 
 
-router.get("/me", protect, (req, res) => {
-  res.json({
-    success: true,
-    userId: req.userId
-  });
+router.get("/me", protect, async(req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+
+    if(!user){
+      return res.status(404).json({
+        succuss:false,
+        message :"user not found"
+      })
+    }
+
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+    
+  }
 });
 
 router.post("/register", registerUser);
--- website/src/Components/Profile/Profile.tsx
+++ website/src/Components/Profile/Profile.tsx
@@ -1,6 +1,13 @@
 import { JSX } from "react"
+import { useContext } from "react"
+import { AuthContext } from "../../Context/AuthContext"
+import { log } from "node:console";
 
 function Profile() :JSX.Element {
+  const {user} = useContext(AuthContext);
+  console.log(user?.name);
+  console.log(user?.email);
+  
   return (
     <div>
     </div>
