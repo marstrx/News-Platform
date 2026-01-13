a7ac661d4e9288a0f974374ed7cc42106f8f2b62
marstrx <marstrone@gmail.com>
2026-01-13 19:48:12 +0100

    user data displayed succussfully

--- website/src/Components/Profile/Profile.tsx
+++ website/src/Components/Profile/Profile.tsx
@@ -1,17 +1,40 @@
-import { JSX } from "react"
+import { JSX, useState } from "react"
 import { useContext } from "react"
 import { AuthContext } from "../../Context/AuthContext"
 import { log } from "node:console";
 
-function Profile() :JSX.Element {
-  const {user} = useContext(AuthContext);
-  console.log(user?.name);
-  console.log(user?.email);
-  
+function Profile(): JSX.Element {
+  const { user } = useContext(AuthContext);
+  const [editing, setEditing] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
-    <div>
+    <div className="h-screen px-5 py-14 bg-blend-multiply">
+      <h2 className="text-4xl font-mono ">Profile information</h2>
+      {!editing ? (
+        <div className="flex justify-center items-center">
+          <div className="flex flex-col justify-center items-start gap-5 px-20 py-28">
+            <div className="flex items-center gap-10">
+              <p className="text-2xl text-emerald-900 font-mono">Full name :</p>
+              <p className="text-2xl text-emerald-900 font-mono">{user?.name}</p>
+            </div>
+            <div className="flex items-center gap-10">
+              <p className="text-2xl text-emerald-900 font-mono">Email :</p>
+              <p className="text-2xl text-emerald-900 font-mono">{user?.email}</p>
+            </div>
+            <button className="bg-[#a43100] rounded text-white p-2 w-full mt-20 font-mono">Edit My Information</button>
+          </div>
+        </div>
+      ) :
+        (<div>
+
+        </div>)
+      }
     </div>
   )
+
 }
 
 export default Profile
