4f683c626caaa2fcfffac3a7a4fa16d4535a583f
marstrx <marstrone@gmail.com>
2026-01-13 19:55:16 +0100

    just change editing to isEditing and toggle added to the btn

--- website/src/Components/Profile/Profile.tsx
+++ website/src/Components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import { log } from "node:console";
 
 function Profile(): JSX.Element {
   const { user } = useContext(AuthContext);
-  const [editing, setEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -13,7 +13,8 @@ function Profile(): JSX.Element {
   return (
     <div className="h-screen px-5 py-14 bg-blend-multiply">
       <h2 className="text-4xl font-mono ">Profile information</h2>
-      {!editing ? (
+      {!isEditing ? (
+        // Display user data
         <div className="flex justify-center items-center">
           <div className="flex flex-col justify-center items-start gap-5 px-20 py-28">
             <div className="flex items-center gap-10">
@@ -24,11 +25,12 @@ function Profile(): JSX.Element {
               <p className="text-2xl text-emerald-900 font-mono">Email :</p>
               <p className="text-2xl text-emerald-900 font-mono">{user?.email}</p>
             </div>
-            <button className="bg-[#a43100] rounded text-white p-2 w-full mt-20 font-mono">Edit My Information</button>
+            <button onClick={()=>setIsEditing(true)} className="bg-[#a43100] rounded text-white p-2 w-full mt-20 font-mono cursor-pointer">Edit My Information</button>
           </div>
         </div>
       ) :
         (<div>
+          {/* Edit Mod */}
 
         </div>)
       }
