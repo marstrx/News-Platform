d77d9b66f793c1edfef34fa3c98c0c11e743a7be
marstrx <marstrone@gmail.com>
2026-01-14 18:08:03 +0100

    update user data

---
+++ controllers/authController/update.controller.js
@@ -0,0 +1,36 @@
+import User from "../../models/user.model";
+
+const updateProfile = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (name) user.name = name;
+    if (email) user.email = email;
+
+    await user.save();
+
+    res.json({
+      success: true,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Update failed",
+    });
+  }
+};
+
+export default updateProfile;
