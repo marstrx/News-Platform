24a93a27f3a0c907d2bc6e15564eb6b76f84901e
marstrx <marstrone@gmail.com>
2026-01-16 17:31:44 +0100

    Guard

---
+++ website/src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { JSX, useContext } from "react"
+import { AuthContext } from "../../Context/AuthContext"
+import { Navigate } from "react-router-dom"
+
+function ProtectedRoute({ children }: { children: JSX.Element }) {
+    const { isAuth } = useContext(AuthContext);
+
+    if (!isAuth) {
+        return <Navigate to={"/login"} replace />
+    }
+
+    return children
+
+}
+
+export default ProtectedRoute
